147b016208cb9630e8df73900c065c1cf5f532c0
Vasya <vasyasilkin@gmail.com>
2025-06-10 23:56:22 +0300

    update

--- grades.cpp
+++ grades.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -5,10 +6,17 @@
 using namespace std;
 
 int main(){
-        
-   ofstream school("subjects.txt", ios::app);
+       int choose;
+       cout << "Add subjects - 1" << endl;
+       cout << "Show subject - 2" << endl;
+ while (true) {
+     cin >> choose;
+       switch (choose) {
+             case 1:{
+       
+   ofstream school_out("subjects.txt", ios::app);
    
-    if (!school.is_open()){
+    if (!school_out.is_open()){
         cout << "ERROR FILE CAN'T OPEN" << endl;
         return 1;
     }
@@ -17,23 +25,37 @@ int main(){
            
        string  subject;
        cout << "Enter subjects (one per line). Enter '0' to finish:\n";
-       while(true){
-             getline (cin, subject);
+        
+       cin >> subject;
 
-       if (subject == "0"){
-           break;
-       }
-       else 
-       {
            if (!subject.empty())
            {
-               school << subject << endl;
-
-       }
+               school_out<< subject << endl;
      }  
-   }
-       school.close();
-       cout << "All subject add in file" << endl;
+       }
+       school_out.close();
+       cout << "Choose next action" << endl;
+    break;
+       }
+    case 2:{
+       ifstream school_in ("subjects.txt");
+       if (!school_in.is_open()){
+           cout << "ERROR FILE CAN'T OPEN" << endl;
+           return 1;
+       }else {
+       string line;
 
+     cout << "It's your subjects" << endl;
+     while (getline(school_in, line)) {
+         cout << line << endl;
+        }
+      school_in.close();
+      }
+    }
+           break;
+    default:
+    cout << "Error, wrong character " << endl;
+    exit(1);
+}
 }
 }
